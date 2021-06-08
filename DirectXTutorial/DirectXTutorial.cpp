a93648f032cff5fb14b8ada68f0765bcda7a7e25
KDBFootman <60589353+KDBFootman@users.noreply.github.com>
2021-06-08 12:20:58 +0900

    Init DirectX11

--- DirectXTutorial/DirectXTutorial.cpp
+++ DirectXTutorial/DirectXTutorial.cpp
@@ -1,4 +1,6 @@
+
 #include <windows.h>
+#include <d3d11_2.h>
 
 
 //--------------------------------------------------------------------------------------
@@ -6,13 +8,23 @@
 //--------------------------------------------------------------------------------------
 HINSTANCE g_hInst = nullptr;
 HWND g_hWnd = nullptr;
+D3D_DRIVER_TYPE g_driverType = D3D_DRIVER_TYPE_NULL;
+D3D_FEATURE_LEVEL g_featureLevel = D3D_FEATURE_LEVEL_11_1;
+ID3D11Device* g_d3dDevice = nullptr;
+ID3D11DeviceContext* g_ImmediateContext = nullptr;
+IDXGISwapChain* g_SwapChain = nullptr;
+ID3D11RenderTargetView* g_RenderTargetView = nullptr;
 
 
 //--------------------------------------------------------------------------------------
 // Forward declarations
 //--------------------------------------------------------------------------------------
 HRESULT InitWindow(HINSTANCE, int);
+HRESULT InitDevice();
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+void Move();
+void Render();
+void CleanupDevice();
 
 
 //--------------------------------------------------------------------------------------
@@ -28,11 +40,25 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLi
 		return 0;
 	}
 
-	MSG msg = {};
-	while (GetMessage(&msg, nullptr, 0, 0)) {
-		TranslateMessage(&msg);
-		DispatchMessage(&msg);
+	if (FAILED(InitDevice())) {
+		CleanupDevice();
+		return 0;
 	}
+
+	MSG msg = {};
+	while (msg.message != WM_QUIT) {
+
+		if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE)) {
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+		else {
+			Move();
+			Render();
+		}
+	}
+
+	CleanupDevice();
 
 	return (int)msg.wParam;
 }
@@ -81,6 +107,96 @@ HRESULT InitWindow(HINSTANCE hInstance, int nCmdShow) {
 
 
 //--------------------------------------------------------------------------------------
+// Create Direct3D device and swap chain
+//--------------------------------------------------------------------------------------
+HRESULT InitDevice() {
+
+	HRESULT hr = S_OK;
+
+	RECT rc = {};
+	GetClientRect(g_hWnd, &rc);
+	UINT width = rc.right - rc.left;
+	UINT height = rc.bottom - rc.top;
+
+	UINT createDeviceFlags = 0;
+#ifdef _DEBUG
+	createDeviceFlags |= D3D11_CREATE_DEVICE_DEBUG;
+#endif
+
+	D3D_DRIVER_TYPE driverTypes[] =
+	{
+		D3D_DRIVER_TYPE_HARDWARE,
+		D3D_DRIVER_TYPE_WARP,
+		D3D_DRIVER_TYPE_REFERENCE
+	};
+	//UINT numDriverTypes = ARRAYSIZE(driverTypes);
+
+	D3D_FEATURE_LEVEL featureLevels[] =
+	{
+		D3D_FEATURE_LEVEL_11_1,
+		D3D_FEATURE_LEVEL_11_0,
+		D3D_FEATURE_LEVEL_10_1,
+		D3D_FEATURE_LEVEL_10_0,
+		//D3D_FEATURE_LEVEL_9_3,
+		//D3D_FEATURE_LEVEL_9_2,
+		//D3D_FEATURE_LEVEL_9_1
+	};
+	UINT numFeatureLevels = ARRAYSIZE(featureLevels);
+
+	DXGI_SWAP_CHAIN_DESC sd = {};
+	sd.BufferDesc.Width = width;
+	sd.BufferDesc.Height = height;
+	sd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	sd.BufferDesc.RefreshRate.Numerator = 60;
+	sd.BufferDesc.RefreshRate.Denominator = 1;
+	sd.SampleDesc.Count = 1;
+	sd.SampleDesc.Quality = 0;
+	sd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+	sd.BufferCount = 2;
+	sd.OutputWindow = g_hWnd;
+	sd.Windowed = TRUE;
+
+	for (auto& driverType : driverTypes) {
+		g_driverType = driverType;
+		hr = D3D11CreateDeviceAndSwapChain(nullptr, g_driverType, nullptr, createDeviceFlags, featureLevels, numFeatureLevels, D3D11_SDK_VERSION, &sd, &g_SwapChain, &g_d3dDevice, &g_featureLevel, &g_ImmediateContext);
+		if (SUCCEEDED(hr)) {
+			break;
+		}
+	}
+	if (FAILED(hr)) {
+		return hr;
+	}
+
+	// Create a render target view
+	ID3D11Texture2D* backBuffer = nullptr;
+	hr = g_SwapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), (LPVOID*)&backBuffer);
+	if (FAILED(hr)) {
+		return hr;
+	}
+
+	hr = g_d3dDevice->CreateRenderTargetView(backBuffer, nullptr, &g_RenderTargetView);
+	backBuffer->Release();
+	if (FAILED(hr)) {
+		return hr;
+	}
+
+	g_ImmediateContext->OMSetRenderTargets(1, &g_RenderTargetView, nullptr);
+
+	// Setup the viewport
+	D3D11_VIEWPORT vp = {};
+	vp.Width = static_cast<FLOAT>(width);
+	vp.Height = static_cast<FLOAT>(height);
+	vp.MinDepth = 0.0f;
+	vp.MaxDepth = 1.0f;
+	vp.TopLeftX = 0;
+	vp.TopLeftY = 0;
+	g_ImmediateContext->RSSetViewports(1, &vp);
+
+	return S_OK;
+}
+
+
+//--------------------------------------------------------------------------------------
 // Called every time the application receives a message
 //--------------------------------------------------------------------------------------
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
@@ -104,4 +220,48 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) 
 	}
 
 	return 0;
+}
+
+
+//--------------------------------------------------------------------------------------
+// Move the frame
+//--------------------------------------------------------------------------------------
+void Move() {
+
+}
+
+
+//--------------------------------------------------------------------------------------
+// Render the frame
+//--------------------------------------------------------------------------------------
+void Render() {
+
+	// Just clear the backbuffer
+	float ClearColor[4] = { 0.0f,0.125f,0.3f,1.0f };
+	g_ImmediateContext->ClearRenderTargetView(g_RenderTargetView, ClearColor);
+
+	g_SwapChain->Present(1, 0);
+}
+
+
+//--------------------------------------------------------------------------------------
+// Clean up the objects we've created
+//--------------------------------------------------------------------------------------
+void CleanupDevice() {
+
+	if (g_ImmediateContext) {
+		g_ImmediateContext->ClearState();
+	}
+	if (g_RenderTargetView) {
+		g_RenderTargetView->Release();
+	}
+	if (g_SwapChain) {
+		g_SwapChain->Release();
+	}
+	if (g_ImmediateContext) {
+		g_ImmediateContext->Release();
+	}
+	if (g_d3dDevice) {
+		g_d3dDevice->Release();
+	}
 }
