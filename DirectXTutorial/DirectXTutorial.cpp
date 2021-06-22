35610df29e069609e1127bdfa17b4de63d7f914c
KDBFootman <60589353+KDBFootman@users.noreply.github.com>
2021-06-22 13:04:06 +0900

    Render second cube Add depth stencil

--- DirectXTutorial/DirectXTutorial.cpp
+++ DirectXTutorial/DirectXTutorial.cpp
@@ -14,9 +14,13 @@
 HINSTANCE				g_hInst = nullptr;
 HWND					g_hWnd = nullptr;
 
+Microsoft::WRL::ComPtr<ID3D11Texture2D> g_DepthStencil = nullptr;
+Microsoft::WRL::ComPtr<ID3D11DepthStencilView> g_DepthStencilView = nullptr;
+
 std::unique_ptr<Device>	g_Device;
 std::unique_ptr<Shader> g_Shader;
 std::unique_ptr<Model> g_Model;
+std::unique_ptr<Model> g_Model2;
 std::unique_ptr<Camera> g_Camera;
 
 //--------------------------------------------------------------------------------------
@@ -133,6 +137,39 @@ HRESULT InitDevice() {
 		return hr;
 	}
 
+	// Create depth stencil texture
+	D3D11_TEXTURE2D_DESC descDepth = {};
+	ZeroMemory(&descDepth, sizeof(descDepth));
+	descDepth.Width = width;
+	descDepth.Height = height;
+	descDepth.MipLevels = 1;
+	descDepth.ArraySize = 1;
+	descDepth.Format = DXGI_FORMAT_D24_UNORM_S8_UINT;
+	descDepth.SampleDesc.Count = 1;
+	descDepth.SampleDesc.Quality = 0;
+	descDepth.Usage = D3D11_USAGE_DEFAULT;
+	descDepth.BindFlags = D3D11_BIND_DEPTH_STENCIL;
+	descDepth.CPUAccessFlags = 0;
+	descDepth.MiscFlags = 0;
+	hr = g_Device->GetD3DDevice()->CreateTexture2D(&descDepth, nullptr, g_DepthStencil.ReleaseAndGetAddressOf());
+	if (FAILED(hr)) {
+		return hr;
+	}
+
+	// Create depth stencil view
+	D3D11_DEPTH_STENCIL_VIEW_DESC descDSV = {};
+	ZeroMemory(&descDSV, sizeof(descDSV));
+	descDSV.Format = descDepth.Format;
+	descDSV.ViewDimension = D3D11_DSV_DIMENSION_TEXTURE2D;
+	descDSV.Texture2D.MipSlice = 0;
+	hr = g_Device->GetD3DDevice()->CreateDepthStencilView(g_DepthStencil.Get(), &descDSV, g_DepthStencilView.ReleaseAndGetAddressOf());
+	if (FAILED(hr)) {
+		return hr;
+	}
+
+	auto renderTarget = g_Device->GetRenderTargetView();
+	g_Device->GetD3DDeviceContext()->OMSetRenderTargets(1, &renderTarget, g_DepthStencilView.Get());
+
 	g_Shader = std::make_unique<Shader>();
 	hr = g_Shader->CreateShader(g_Device->GetD3DDevice());
 	if (FAILED(hr)) {
@@ -141,6 +178,12 @@ HRESULT InitDevice() {
 
 	g_Model = std::make_unique<Model>();
 	hr = g_Model->CreateModel(g_Device->GetD3DDevice());
+	if (FAILED(hr)) {
+		return hr;
+	}
+
+	g_Model2 = std::make_unique<Model>();
+	hr = g_Model2->CreateModel(g_Device->GetD3DDevice());
 	if (FAILED(hr)) {
 		return hr;
 	}
@@ -212,6 +255,14 @@ void Move() {
 		return DirectX::XMMatrixRotationY(t);
 	});
 
+	g_Model2->MoveModel([]()
+	{
+		const auto mScale = DirectX::XMMatrixScaling(0.3f, 0.3f, 0.3f);
+		const auto mSpin = DirectX::XMMatrixRotationZ(-t);
+		const auto mTranslate = DirectX::XMMatrixTranslation(-3.0f, 0.0f, 0.0f);
+		const auto mOrbit = DirectX::XMMatrixRotationY(-t * 2.0f);
+		return mScale * mSpin * mTranslate * mOrbit;
+	});
 }
 
 
@@ -230,6 +281,13 @@ void Render() {
 
 	g_Model->RenderModel(g_Device->GetD3DDeviceContext());
 
+	// Render second cube
+	auto world2 = g_Model2->GetWorld();
+
+	g_Shader->SetRenderShader(g_Device->GetD3DDeviceContext(), world2, view, projection);
+
+	g_Model2->RenderModel(g_Device->GetD3DDeviceContext());
+
 	g_Device->Present();
 }
 
@@ -244,7 +302,11 @@ void Clear() {
 
 	float ClearColor[4] = { 0.0f,0.125f,0.3f,1.0f };
 	context->ClearRenderTargetView(renderTarget, ClearColor);
-	context->OMSetRenderTargets(1, &renderTarget, nullptr);
+
+	// Clear the depth buffer to 1.0 (max depth)
+	context->ClearDepthStencilView(g_DepthStencilView.Get(), D3D11_CLEAR_DEPTH, 1.0f, 0);
+
+	context->OMSetRenderTargets(1, &renderTarget, g_DepthStencilView.Get());
 
 	// Set the viewport
 	auto viewport = g_Device->GetScreenViewport();
@@ -260,7 +322,11 @@ void CleanupDevice() {
 	auto context = g_Device->GetD3DDeviceContext();
 	context->ClearState();
 
+	g_DepthStencil.Reset();
+	g_DepthStencilView.Reset();
+
 	g_Camera.reset();
+	g_Model2.reset();
 	g_Model.reset();
 	g_Shader.reset();
 }
